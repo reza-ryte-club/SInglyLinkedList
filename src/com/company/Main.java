4f585b8a32e3b0382bd5d0176bb27fb27ad5b067
Reza <mrezaurrahman@live.com>
2015-06-18 00:21:52 +0600

    added remove duplication code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Hashtable;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +11,9 @@ public class Main {
         Node lList = new Node(3);
         lList.appendToTail(8);
         lList.appendToTail(6);
+        lList.appendToTail(8);
+        lList.appendToTail(12);
+        lList.appendToTail(12);
         lList.appendToTail(12);
 
         int in = lList.nodeLength();
@@ -17,13 +22,43 @@ public class Main {
         in = lList.nodeLength();
         System.out.println(in);
 
-        while(lList!=null){
-            System.out.println(lList.data);
-            lList = lList.next;
-        }
+        travarseList(lList);
+        removeDuplicate(lList);
+        travarseList(lList);
 
+
+        
 
 //        System.out.println(lList.next.next.data);
 
     }
+    
+    
+     static void travarseList(Node n){
+        while(n!=null){
+            System.out.println(n.data);
+            n = n.next;
+        }
+         
+    }
+
+    static void removeDuplicate(Node n){
+        Hashtable table = new Hashtable();
+        Node previous =     null;
+        while(n!=null) {
+            if (table.containsKey(n.data)) {
+                previous.next = n.next; // This sets the address of previous node's next  to the next node, hence skips this node
+                        
+            }
+            else {
+                table.put(n.data,"poo");
+                previous = n;
+                
+            }
+            n = n.next;
+            }
+
+        }
+
+
 }
