c1edf8d57797a17bd0f182c77525efcd8ee4afc0
Md Rezaur Rahman <mrezaurrahman@live.com>
2015-08-23 18:07:46 +0600

    Update Node.java

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,54 +1,50 @@
 package com.company;
 
 /**
- * Created by rahmanmdrezaur on 6/17/15.
+ * Created by reza on 8/23/15.
  */
 public class Node {
     Node next = null;
     int data;
 
-    public Node(int d) {
+    public Node(int d){
         data = d;
     }
 
-    void appendToTail(int d) {
+    void appendToTail(int d){
         Node end = new Node(d);
         Node n = this;
-        while (n.next != null) n = n.next;
-        n.next = end;
+        while(n.next!=null){
+            n = n.next;
+        }
 
+        n.next = end;
     }
+
 
     public int nodeLength(){
         Node temp = this;
         int counter = 0;
-        while (temp!= null) {
+        while(temp!=null){
             temp = temp.next;
-            counter++;
+          counter++;
         }
-
-        return counter;
-
+    return counter;
     }
 
 
-    public Node deleteNode(Node head, int d){
-        Node n = head;
-        if(n.data==d){
+    public void deleteNode(int d){
+        Node n = this;
+        if(n.data == d){
             n.next = n.next.next;
-            return head.next;
         }
-
         while(n.next!=null){
-            if(n.next.data == d ){
+            if(n.next.data==d){
                 n.next = n.next.next;
-                return head;
-
             }
             n = n.next;
-
         }
-        return head;
+
     }
 
 
